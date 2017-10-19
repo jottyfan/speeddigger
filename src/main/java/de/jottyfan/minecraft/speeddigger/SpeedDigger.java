930d6bdcc935c3b369aa7dbb540cc3434fad10e2
jotty <jottyfan@gmx.de>
2017-10-19 15:23:45 +0200

    renamings

---
+++ src/main/java/de/jottyfan/minecraft/speeddigger/SpeedDigger.java
@@ -0,0 +1,29 @@
+package de.jottyfan.minecraft.speeddigger;
+
+import de.jottyfan.minecraft.speeddigger.help.SpeedDiggerTabs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * 
+ * @author jotty
+ *
+ */
+@Mod(modid = SpeedDigger.MODID, version = SpeedDigger.VERSION)
+public class SpeedDigger {
+	public static final String MODID = "speeddigger";
+	public static final String VERSION = "1.12.2.0";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		SpeedDiggerTabs tabs = new SpeedDiggerTabs();
+		// TODO: to be set on created items
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+}
---
+++ src/main/java/de/jottyfan/minecraft/speeddigger/help/SpeedDiggerTabs.java
@@ -0,0 +1,29 @@
+package de.jottyfan.minecraft.speeddigger.help;
+
+import de.jottyfan.minecraft.speeddigger.SpeedDigger;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * 
+ * @author jotty
+ *
+ */
+public class SpeedDiggerTabs extends CreativeTabs {
+	
+	public SpeedDiggerTabs() {
+		super(SpeedDigger.MODID);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public ItemStack getTabIconItem() {
+		StringBuilder buf = new StringBuilder(SpeedDigger.MODID);
+		buf.append(":").append("speeddiggericon");
+		Item item = Item.getByNameOrId(buf.toString());
+		return new ItemStack(item);
+	}
+}
